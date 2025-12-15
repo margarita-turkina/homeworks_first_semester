5e3d7222418ca8f817b498b4fa63d6eeae6645c7
margarita-turkina <turkina.ms@phystech.edu>
2025-12-15 22:06:35 +0300

    Create MainExample.java

---
+++ src/test/java/MainExample.java
@@ -0,0 +1,18 @@
+public class MainExample {
+    public static void main(String[] args) {
+        final var service = new ValidationDecorator(
+            new MetricableDecorator(
+                new LoggingDecorator(
+                    new CachingDecorator(
+                        new SimpleDataService()
+                    )
+                )
+            )
+        );
+
+        service.saveData("key", "data");
+        final var data = service.findDataByKey("key");
+        service.deleteData("key");
+        final var noData = service.findDataByKey("key");
+    }
+}
